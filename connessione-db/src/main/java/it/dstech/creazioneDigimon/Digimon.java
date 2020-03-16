e73110b6a01c7226d1546f66d6e892a4011b2a9b
Admin <Admin@Matteo>
2020-03-16 19:57:22 +0100

    Inserimento classi enum e creazione main dello sfidante

---
+++ connessione-db/src/main/java/it/dstech/creazioneDigimon/Digimon.java
@@ -0,0 +1,13 @@
+package it.dstech.creazioneDigimon;
+
+public class Digimon {
+
+	private String nome;
+	private int HP;
+	private int ATK;
+	private int DEF;
+	private int RES;
+	StadioEvolutivo Evo;
+	Tipo tipo;
+	
+}
